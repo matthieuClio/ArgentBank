7f6d98f300348f230a7f423daeaa62ee9b1fbfa9
Matthieu <matthieu.clio@gmail.com>
2024-01-22 17:17:54 +0100

    Feat : add header layout for navigation.

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,7 +7,14 @@ export default function Header () {
             <nav className="main-navigation">
                 <ul className="main-navigation__ul">
                     <li>
-                        <img src="./images/argentBankLogo.png" alt="logo ARGENTBANK" />
+                        <img src="./images/argentBankLogo.png" alt="logo ARGENTBANK" className="main-navigation__ul__logo"/>
+                    </li>
+
+                    <li className="main-navigation__ul__sign-in">
+                        <a href="#">
+                            <span className="fa fa-user-circle main-navigation__ul__sign-in__icon"></span>
+                            <span>Sign In</span>
+                        </a>
                     </li>
                 </ul>
             </nav>
